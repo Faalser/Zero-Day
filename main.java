e41be274836dd515b8c9137cfdac544e0ebc2a41
Faalser <thnhubert@gmail.com>
2025-12-17 16:05:11 +0100

    ajout du chiffrage en morse

--- main.java
+++ main.java
@@ -47,4 +47,68 @@ class main extends Program {
             write("Salut !", 25);
         }
     }
+
+    // les programmes suivant chiffrent les questions selon différentes méthodes
+
+    // chiffrage en morse
+    void toMorse(String questions) {
+        String res = "";
+        for (int i = 0; i < length(questions); i++) {
+            char c = charAt(questions, i);
+            if (c == 'A' || c == 'a') {
+                res = res + ".-";
+            } else if (c == 'B' || c == 'b') {
+                res = res + "-...";
+            } else if (c == 'C' || c == 'c') {
+                res = res + "-.-.";
+            } else if (c == 'D' || c == 'd') {
+                res = res + "-..";
+            } else if (c == 'E' || c == 'e') {
+                res = res + ".";
+            } else if (c == 'F' || c == 'f') {
+                res = res + "..-.";
+            } else if (c == 'G' || c == 'g') {
+                res = res + "--.";
+            } else if (c == 'H' || c == 'h') {
+                res = res + "....";
+            } else if (c == 'I' || c == 'i') {
+                res = res + "..";
+            } else if (c == 'J' || c == 'j') {
+                res = res + ".---";
+            } else if (c == 'K' || c == 'k') {
+                res = res + "-.-";
+            } else if (c == 'L' || c == 'l') {
+                res = res + ".-..";
+            } else if (c == 'M' || c == 'm') {
+                res = res + "--";
+            } else if (c == 'N' || c == 'n') {
+                res = res + "-.";
+            } else if (c == 'O' || c == 'o') {
+                res = res + "---";
+            } else if (c == 'P' || c == 'p') {
+                res = res + ".--.";
+            } else if (c == 'Q' || c == 'q') {
+                res = res + "--.-";
+            } else if (c == 'R' || c == 'r') {
+                res = res + ".-.";
+            } else if (c == 'S' || c == 's') {
+                res = res + "...";
+            } else if (c == 'T' || c == 't') {
+                res = res + "-";
+            } else if (c == 'U' || c == 'u') {
+                res = res + "..-";
+            } else if (c == 'V' || c == 'v') {
+                res = res + "...-";
+            } else if (c == 'W' || c == 'w') {
+                res = res + ".--";
+            } else if (c == 'X' || c == 'x') {
+                res = res + "-..-";
+            } else if (c == 'Y' || c == 'y') {
+                res = res + "-.--";
+            } else if (c == 'Z' || c == 'z') {
+                res = res + "--..";
+            res = res + " ";
+        }
+    }
+
 }
